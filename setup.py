4faddbc87736b634ec00688d8a36b2d140023097
David Bell <dave@evad.info>
2015-07-28 21:52:40 +0100

    Updated to new /etc/acc and netgroup C interface

--- setup.py
+++ setup.py
@@ -3,15 +3,17 @@
 import distutils.core
 
 distutils.core.setup(name='acc',
-      version='10',
+      version='12',
       license='GPL3',
       description='A system to manage the pam_access access.conf file',
       long_description='''This tool is a easy to use front-end to allow scripts
       and system administrators to build and maintain an access.conf file for use
       with pam_access.so. The tool is designed so that scripts and humans don't
       trample on each other.''',
-      url='http://www.evad.info',
+      url='http://www.evad.io',
       author='David Bell',
-      author_email='dave@evad.info',
+      author_email='dave@evad.io',
       scripts=['acc'],
+options = {'bdist_rpm':{'post_install' : 'post_install',
+                        'post_uninstall' : 'post_uninstall'}},
       )
